0a51ecea12940b13c2dc9e353014b39c6c9d79ac
Tomas Kravcik <kravcik@procorp.cz>
2025-02-05 18:43:55 +0100

    add files

---
+++ src/components/Pizza.js
@@ -0,0 +1,25 @@
+import Data from "./PizzaBox"
+import {useContext} from "react";
+
+export default function Pizza() {
+
+    const {name} = useContext(Data)
+    return (
+        <Data.Consumer >
+
+
+            {
+                (data) => {
+                    return (
+                        <div>
+                            <p>{name}</p>
+                            {data.name}
+                        </div>
+                    )
+                }
+            }
+
+            
+        </Data.Consumer>
+    )
+}
