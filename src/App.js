8532e725fe2aebc231f1fd18bca6a9f8f6fdc660
Tomas Kravcik <kravcik@procorp.cz>
2025-02-05 19:02:34 +0100

    setup store + provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import PizzaBox from "./components/PizzaBox";
+import {Provider} from 'react-redux'
+import store from "./components/redux/store";
 
 function App() {
     return (
-        <>
-            Hello
-        </>
+        <Provider store={store}>
+            <PizzaBox/>
+        </Provider>
     );
 }
 
