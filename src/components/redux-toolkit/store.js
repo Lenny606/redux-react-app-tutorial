b28f4ffbe1968b8d838a4fc5924a2b8943e78160
Tomas Kravcik <kravcik@procorp.cz>
2025-02-11 19:36:11 +0100

    redux toolkit middleware use

--- src/components/redux-toolkit/store.js
+++ src/components/redux-toolkit/store.js
@@ -1,11 +1,16 @@
 import {pizzaSlice} from "./pizza/pizzaSlice";
 import {configureStore} from '@reduxjs/toolkit'
 import {burgerSlice} from "./burger/burgerSlice";
+import {createLogger } from "redux-logger";
 
+const logger = createLogger()
 export const storeToolkit = configureStore({
     reducer: {
         pizza: pizzaSlice.reducer,
         burger: burgerSlice.reducer
         // other reducers if needed
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(logger)
     }
 });
