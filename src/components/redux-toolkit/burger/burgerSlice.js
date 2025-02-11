460547e4fd19da9c2d1729f5a82f10f3b7b335cb
Tomas Kravcik <kravcik@procorp.cz>
2025-02-11 19:50:07 +0100

    multiple reducers in slice

--- src/components/redux-toolkit/burger/burgerSlice.js
+++ src/components/redux-toolkit/burger/burgerSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit'
-
+import {pizzaSlice} from "../pizza/pizzaSlice"
 const initialState = {
     burgerBase: 1000
 }
@@ -11,6 +11,13 @@ export const burgerSlice = createSlice({
         burger_order: (state) => {
             return state.burgerBase - 1
         }
+    },
+    //for multiple reducers use extraReducer() => if pizza is ordered, 1 burger is free
+    extraReducers: (builder) => {
+        builder.addCase(pizzaSlice.actions.pizza_order, (state) => {
+            return state.burgerBase - 1
+        })
     }
+
 
 });
